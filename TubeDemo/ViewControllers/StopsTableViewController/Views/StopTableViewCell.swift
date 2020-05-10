920e7bf366454b7036d0c65aba0d3f82b5563ef1
stringcode <michael@stringcode.co.uk>
2020-05-10 14:59:30 +0100

    minor clean up

--- TubeDemo/ViewControllers/StopsTableViewController/Views/StopTableViewCell.swift
+++ TubeDemo/ViewControllers/StopsTableViewController/Views/StopTableViewCell.swift
@@ -18,11 +18,6 @@ class StopTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-        
-    }
     
     override func layoutSubviews() {
         super.layoutSubviews()
